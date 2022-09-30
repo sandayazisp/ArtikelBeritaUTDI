f240a09270c04a1a1088fe6da7bd8e474de84774
sanday <sandayazisp12@gmail.com>
2022-09-30 16:41:14 +0700

    Menampilkan daftar menggunakan RecyclerView

--- app/src/main/java/id/sandayazisp/artikelberitautdi/MainActivity.kt
+++ app/src/main/java/id/sandayazisp/artikelberitautdi/MainActivity.kt
@@ -3,13 +3,22 @@ package id.sandayazisp.artikelberitautdi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import id.sandayazisp.artikelberitautdi.adapter.ItemAdapter
 import id.sandayazisp.artikelberitautdi.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val textView: TextView = findViewById(R.id.berita)
-        textView.text = Datasource().loadArtikels().size.toString()
+        // Initialize data.
+        val myDataset = Datasource().loadArtikels()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
     }
 }
---
+++ app/src/main/java/id/sandayazisp/artikelberitautdi/adapter/ItemAdapter.kt
@@ -0,0 +1,35 @@
+package id.sandayazisp.artikelberitautdi.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import id.sandayazisp.artikelberitautdi.model.Artikel
+import id.sandayazisp.artikelberitautdi.R
+
+class ItemAdapter(
+    private val context: Context,
+    private val dataset: List<Artikel>
+) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.item_title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        // create a new view
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent,false)
+
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.textView.text = context.resources.getString(item.stringResourceId)
+    }
+
+    override fun getItemCount() = dataset.size
+}
